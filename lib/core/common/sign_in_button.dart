51a84f42bf609eda8662f3e097c3e3e3fa617685
Hưng Phan <hungpb912@gmail.com>
2024-02-22 16:12:39 +0700

    init project, set up firebase

---
+++ lib/core/common/sign_in_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:reddit_clone/core/constants/constants.dart';
+import 'package:reddit_clone/theme/pallete.dart';
+
+class SignInButton extends StatelessWidget {
+  const SignInButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ElevatedButton.icon(
+        onPressed: () {},
+        icon: Image.asset(
+          Constants.googlePath,
+          width: 35,
+        ),
+        label: const Text(
+          "Continue with Google",
+          style: TextStyle(fontSize: 18),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Pallete.greyColor,
+          minimumSize: const Size(double.infinity, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+    );
+  }
+}
